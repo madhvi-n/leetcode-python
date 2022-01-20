522ddbbe983e0201115a8699c8cb5068cae45b6e
Madhvi <30744898+MayaScarlet@users.noreply.github.com>
2022-01-20 15:17:02 +0530

    Time: 276 ms (77.76%), Space: 29.9 MB (16.89%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -4,9 +4,6 @@ class ProductOfNumbers:
         self.arr = []
         self.zero_index = -1
     
-
-    
-    
     def add(self, num: int) -> None:
         if num != 0 and len(self.arr) > 0:
             last_number = self.arr[-1]
