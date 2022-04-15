7f0c56047a1e3305c9257eab94d56dcb2e8b65d9
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-04-15 12:03:59 +0530

    Time: 117 ms (90.41%), Space: 15.1 MB (97.31%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -2,28 +2,27 @@ from collections import defaultdict
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        hashMap, res, pStart, sStart = defaultdict(int), [], len(p), len(s)
-        if pStart > sStart : return []
-
-        # build hashmap for pattern
-        for character in p:
-            hashMap[character] += 1
-
-        # initial full pass over the window
-        for i in range(pStart - 1):
-            if s[i] in hashMap:
-                hashMap[s[i]] -= 1
-
-        # slide the window with stride 1
-        for i in range(-1, sStart - pStart + 1):
-            if i > -1 and s[i] in hashMap:
-                hashMap[s[i]] += 1
-
-            if i + pStart < sStart and s[i + pStart] in hashMap:
-                hashMap[s[i + pStart]] -= 1
-
-            # check whether we encountered an anagram
-            if all(v == 0 for v in hashMap.values()):
-                res.append(i + 1)
-
+        if len(p) > len(s):
+            return []
+        
+        pCount, sCount = {}, {}
+        left = 0
+        
+        for i in range(len(p)):
+            pCount[p[i]] = 1 + pCount.get(p[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+        
+        res = [0] if pCount == sCount else []
+        
+        for right in range(len(p), len(s)):
+            sCount[s[right]] = 1 + sCount.get(s[right], 0)
+            sCount[s[left]] -= 1
+            
+            if sCount[s[left]] == 0:
+                sCount.pop(s[left])
+            
+            left += 1
+            
+            if pCount == sCount:
+                res.append(left)
         return res
