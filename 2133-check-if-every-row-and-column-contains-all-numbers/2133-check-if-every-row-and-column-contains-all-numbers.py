cdf07f30a0990d659a96a0ffb60abb5b6638a205
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-04-17 11:15:21 +0530

    Time: 866 ms (77.75%), Space: 15.9 MB (5.22%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,17 +1,17 @@
 class Solution:
-    def checkValid(self, board: List[List[int]]) -> bool:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
         cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
         
         
-        for r in range(len(board)):
-            for c in range(len(board)):
+        for r in range(len(matrix)):
+            for c in range(len(matrix)):
                 #If element exist in any of the three sets, return False
-                if board[r][c] in rows[r] or board[r][c] in cols[c]:
+                if matrix[r][c] in rows[r] or matrix[r][c] in cols[c]:
                     return False
                 
                 #Add element if it doesn't exist
-                rows[r].add(board[r][c])
-                cols[c].add(board[r][c])
+                rows[r].add(matrix[r][c])
+                cols[c].add(matrix[r][c])
                 
         return True
