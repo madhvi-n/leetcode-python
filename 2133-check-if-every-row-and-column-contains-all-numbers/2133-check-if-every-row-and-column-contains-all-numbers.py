6fa6a20d36ffbd1620b16d4f47517e1c4b5a81ef
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-04-17 11:13:50 +0530

    Time: 901 ms (70.89%), Space: 15.8 MB (14.23%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkValid(self, board: List[List[int]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        
+        
+        for r in range(len(board)):
+            for c in range(len(board)):
+                #Ignore empty cells
+                if board[r][c] == ".":
+                    continue
+                
+                #If element exist in any of the three sets, return False
+                if board[r][c] in rows[r] or board[r][c] in cols[c]:
+                    return False
+                
+                #Add element if it doesn't exist
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                
+        return True
