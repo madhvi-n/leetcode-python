6cf08f128cef86aea0bc25df2f8c7c7e9d7e7586
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-04-26 18:41:54 +0530

    Time: 887 ms (14.70%), Space: 61.4 MB (36.44%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -17,7 +17,7 @@ class RandomizedSet:
         return False
 
     def getRandom(self) -> int:
-        return self.data[random.randint(0, len(self.data) - 1)]
+        return random.choice(self.data)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
