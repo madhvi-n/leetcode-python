4ad4fb56a258f30d9e6a46747955e0f6b5bac5ca
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-05-14 20:23:34 +0530

    Time: 214 ms (31.43%), Space: 20.6 MB (84.05%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
         count = 0
-        for i, j in enumerate(citations):
+        for i, j in enumerate(citations[::-1]):
             if i < j:
                 count += 1
         return count
