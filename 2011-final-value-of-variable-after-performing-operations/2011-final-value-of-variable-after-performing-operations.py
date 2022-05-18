f508f675e6ee596bb47953b516f0826301765b97
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-05-18 17:39:25 +0530

    Time: 92 ms (19.14%), Space: 14 MB (12.43%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -2,9 +2,19 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x = 0
         
-        for operation in operations:
+        # for operation in operations:
+        #     if operation == "X++" or operation == "++X":
+        #         x += 1
+        #     else:
+        #         x -= 1
+        # return x
+        
+        counter = collections.Counter(operations)
+        print(counter)
+        
+        for operation, count in counter.items():
             if operation == "X++" or operation == "++X":
-                x += 1
+                x += (1 * counter.get(operation))
             else:
-                x -= 1
+                x -= (1 * counter.get(operation))
         return x
