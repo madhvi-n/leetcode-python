393434b0f4ed2240c6f2d3d4d65a18f1d650a189
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-06-04 15:17:04 +0530

    Time: 314 ms (46.31%), Space: 21.3 MB (30.34%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.sub = {}
+        self.suggestion = []
+        
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        root = TrieNode()
+        for product in sorted(products):
+            self._insert(product, root)
+        return self._search(searchWord, root)
+    
+    def _insert(self, product: str, root: TrieNode) -> None:
+        trie = root
+        for char in product:
+            if char not in trie.sub:
+                trie.sub[char] = TrieNode()
+            trie = trie.sub[char]
+            trie.suggestion.append(product)
+            trie.suggestion.sort()
+            if len(trie.suggestion) > 3:
+                trie.suggestion.pop()
+    
+    def _search(self, searchWord: str, root: TrieNode) -> List[List[str]]:              
+        ans = []
+        for char in searchWord:
+            if root:
+                root = root.sub.get(char)
+            ans.append(root.suggestion if root else [])            
+        return ans
