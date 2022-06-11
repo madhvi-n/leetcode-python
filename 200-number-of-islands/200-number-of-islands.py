9a57c26aba64b652e9ec149eb4234c064afba332
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-06-11 18:58:58 +0530

    Time: 579 ms (17.85%), Space: 16.5 MB (47.55%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
+
+        def mark_current_island(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] != "1":
+                return
+
+            grid[row][col] = "2"
+            mark_current_island(row + 1, col)
+            mark_current_island(row - 1, col)
+            mark_current_island(row, col + 1)
+            mark_current_island(row, col - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    mark_current_island(r, c)
+                    islands += 1
+        return islands
