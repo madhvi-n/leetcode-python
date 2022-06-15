c048738be0dcd1a3ba2ca12c2f2bd84ffdb9da04
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-06-15 15:03:01 +0530

    Time: 164 ms (36.61%), Space: 15.5 MB (86.90%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,55 +1,22 @@
-from collections import defaultdict
 class Solution:
+    def findOrder(self, num_courses: int, prerequisites: List[List[int]]) -> List[int]:
+        adjacency_list = defaultdict(list)
+        indegree = dict()
+        topological_order = []
 
-    WHITE = 1
-    GRAY = 2
-    BLACK = 3
-
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-
-        # Create the adjacency list representation of the graph
-        adj_list = defaultdict(list)
-
-        # A pair [a, b] in the input represents edge from b --> a
         for dest, src in prerequisites:
-            adj_list[src].append(dest)
+            adjacency_list[src].append(dest)
+            indegree[dest] = 1 + indegree.get(dest, 0)
 
-        topological_sorted_order = []
-        is_possible = True
+        queue = deque([k for k in range(num_courses) if k not in indegree])
 
-        # By default all vertces are WHITE
-        color = {k: Solution.WHITE for k in range(numCourses)}
-        def dfs(node):
-            nonlocal is_possible
+        while queue:
+            vertex = queue.popleft()
+            topological_order.append(vertex)
 
-            # Don't recurse further if we found a cycle already
-            if not is_possible:
-                return
-
-            # Start the recursion
-            color[node] = Solution.GRAY
-
-            # Traverse on neighboring vertices
-            if node in adj_list:
-                for neighbor in adj_list[node]:
-                    if color[neighbor] == Solution.WHITE:
-                        dfs(neighbor)
-                    elif color[neighbor] == Solution.GRAY:
-                         # An edge to a GRAY vertex represents a cycle
-                        is_possible = False
-
-            # Recursion ends. We mark it as black
-            color[node] = Solution.BLACK
-            topological_sorted_order.append(node)
-
-        for vertex in range(numCourses):
-            # If the node is unprocessed, then call dfs on it.
-            if color[vertex] == Solution.WHITE:
-                dfs(vertex)
-
-        return topological_sorted_order[::-1] if is_possible else []
+            if vertex in adjacency_list:
+                for node in adjacency_list[vertex]:
+                    indegree[node] -= 1
+                    if indegree[node] == 0:
+                        queue.append(node)
+        return topological_order if len(topological_order) == num_courses else []
