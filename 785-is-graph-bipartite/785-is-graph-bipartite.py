0e50816364843174073d11b41e68b5cd1c6a2f1d
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-06-20 21:52:25 +0530

    Time: 320 ms (20.32%), Space: 14.8 MB (6.91%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        adjacency_list = defaultdict(list)
+        n = len(graph)
+        
+        for index in range(n):
+            if graph[index]:
+                adjacency_list[index].extend(graph[index])
+        
+        print(adjacency_list)
+        
+        visited = [False] * n
+        color = [False] * n
+        
+        src = 0
+        visited[src] = True
+        
+        def dfs(v):
+            for u in graph[v]:
+                if not visited[u]:
+                    visited[u] = True
+                    color[u] = not color[v]
+                    
+                    if not dfs(u):
+                        return False
+                    
+                elif color[v] == color[u]:
+                    return False
+            return True
+        
+        for i in range(n):
+            if not dfs(i):
+                return False
+        return True
