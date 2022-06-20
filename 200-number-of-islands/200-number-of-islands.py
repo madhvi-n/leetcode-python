f2b875e87d2164145eb73b4560113e1135fda279
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-06-20 18:38:32 +0530

    Time: 615 ms (15.71%), Space: 25.6 MB (8.81%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -6,11 +6,14 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         islands = 0
-
+        visited = set()
+        
         def mark_current_island(row, col):
             if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] != "1":
                 return
-
+            
+            visited.add((row, col))
+            
             grid[row][col] = "2"
             mark_current_island(row + 1, col)
             mark_current_island(row - 1, col)
@@ -19,7 +22,7 @@ class Solution:
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1":
+                if grid[r][c] == "1" and (r, c) not in visited:
                     mark_current_island(r, c)
                     islands += 1
         return islands
