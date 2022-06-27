399e70738fe281fe8c01a1a72658c56bd2164e03
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-06-27 21:17:01 +0530

    Time: 242 ms (32.20%), Space: 16.7 MB (46.85%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -5,10 +5,13 @@ class Solution:
         
         rows = len(grid)
         cols = len(grid[0])
-        count = 0
+        max_region = 0
         
         def mark_current_island(row, col):
-            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] != 1:
+            if row < 0 or col < 0 or row >= rows or col >= cols:
+                return 0
+            
+            if grid[row][col] == 0 or grid[row][col] == 2:
                 return 0
             
             grid[row][col] = 2
@@ -22,5 +25,6 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    count = max(mark_current_island(r, c), count)        
-        return count
+                    curr_size = mark_current_island(r, c)
+                    max_region = max(max_region, curr_size)       
+        return max_region
