b354e63658f6c1abf0167eb51ac74945df0e0c4f
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-01 10:50:31 +0530

    Time: 379 ms (21.38%), Space: 15 MB (6.27%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -2,22 +2,15 @@ class Solution:
     def findCircleNum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
         visited = set()
-        adjacency = defaultdict(list)
         
-        for i in range(n):
-            for j in range(n):
-                if matrix[i][j] == 1:
-                    adjacency[i+1].append(j+1)
-                    adjacency[j+1].append(i+1)
+        def dfs(i):
+            for j in range(n) :
+                if matrix[i][j] == 1 and j not in visited:
+                    visited.add(j)
+                    dfs(j)
         
-        def dfs(source):
-            for adjacent in adjacency[source]:
-                if adjacent not in visited:
-                    visited.add(adjacent)
-                    dfs(adjacent)
         ans = 0
-        
-        for i in range(1, n+1):
+        for i in range(n):
             if i not in visited:
                 dfs(i)
                 ans += 1
