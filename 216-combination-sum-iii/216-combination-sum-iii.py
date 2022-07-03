f6307d8a7701c56a17a0651294ad69860a7b0c0b
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-03 17:12:15 +0530

    Time: 65 ms (12.37%), Space: 13.6 MB (99.95%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,9 +1,21 @@
 class Solution:
+    
+    def backtrack(self, length, curr_target, combination):
+        if curr_target < 0 or length < 0: 
+            return
+        
+        if curr_target == 0 and length == 0:
+            self.result.append(combination)
+            
+        start = combination[-1] + 1 if combination else 1
+            
+        for i in range(start, 10):
+            self.backtrack(length - 1, curr_target - i, combination + [i])
+
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def helper(k, n, cap):
-            if not k:
-                return [[]] * (not n)
-            return [comb + [last]
-                    for last in range(1, cap)
-                    for comb in helper(k-1, n-last, last)]
-        return helper(k, n, 10)
+        self.result = []
+        self.backtrack(k, n, [])
+        return self.result
+        
+            
+                
