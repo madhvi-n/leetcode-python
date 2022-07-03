972f654525d1bdee2babfb3aba93f34004456566
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-03 13:50:22 +0530

    Time: 189 ms (18.08%), Space: 14.2 MB (11.00%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,20 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []                                 
+        combination = []
         
-        def dfs(i, currentArray, total):                   
+        def dfs(i, combination, total):
+            # base cases: if total reaches target val, append a copy to result
+            # if index is out of bounds, or total is higher than target, return
             if total == target:
-                result.append(currentArray.copy())
+                result.append(combination[:])
                 return
             
             if i >= len(candidates) or total > target:
                 return
             
-            currentArray.append(candidates[i])
-            dfs(i, currentArray, total + candidates[i])
+            # include the current candidate in the combination and call dfs with total + candidates[i]
+            combination.append(candidates[i])
+            dfs(i, combination, total + candidates[i])
             
-            currentArray.pop()            
-            dfs(i + 1, currentArray, total)
+            # exclude the current candidate and call dfs over next index
+            combination.pop()            
+            dfs(i + 1, combination, total)
             
         dfs(0, [], 0)
         return result
