129475ccbab72aa713ae9f4c350aed38651ab771
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-05 09:15:27 +0530

    Time: 236 ms (8.63%), Space: 19.6 MB (14.51%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,37 @@
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
+#         hashmap = {}
+        
+#         for num in nums:
+#             hashmap[num] = 1 + hashmap.get(num, 0)
+        
+#         res = dict(sorted(hashmap.items(), key=lambda x: x[1], reverse=True))
+        
+#         result = []
+        
+#         for x in res.keys():
+#             result.append(x)
+#             k -= 1
+#             if k == 0:
+#                 break
+#         return result
+
+        count = dict()
+        frequency = [[] for i in range(len(nums) + 1)]
         
         for num in nums:
-            hashmap[num] = 1 + hashmap.get(num, 0)
+            count[num] = 1 + count.get(num, 0)
+            
+        for n, c in count.items():
+            frequency[c].append(n)
         
-        res = dict(sorted(hashmap.items(), key=lambda x: x[1], reverse=True))
+        res = []
         
-        result = []
-        
-        for x in res.keys():
-            result.append(x)
-            k -= 1
-            if k == 0:
-                break
-        return result
+        for i in range(len(frequency) - 1, 0, -1):
+            for n in frequency[i]:
+                res.append(n)
+                
+                if len(res) == k:
+                    return res
