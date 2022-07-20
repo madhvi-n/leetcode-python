1997ad9b06f1b341b112969b08772676935015ad
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-20 12:51:04 +0530

    Time: 2236 ms (7.81%), Space: 37.2 MB (6.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,4 +1,24 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums) - k]
+        # nums.sort()
+        # return nums[len(nums) - k]
+        
+        k = len(nums) - k
+        
+        def quickselect(left, right):
+            pivot, p = nums[right], left
+            
+            for i in range(left, right):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            
+            nums[p], nums[right] = nums[right], nums[p]
+            
+            if p > k:
+                return quickselect(left, p - 1)
+            elif p < k:
+                return quickselect(p + 1, right)
+            else:
+                return nums[p]
+        return quickselect(0, len(nums) - 1)
