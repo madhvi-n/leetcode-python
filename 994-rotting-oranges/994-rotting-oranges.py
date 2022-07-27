dbbe9b79d191755962e738515d7e6dd33209bf9d
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-27 14:29:34 +0530

    Time: 97 ms (26.73%), Space: 13.9 MB (91.19%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -4,7 +4,11 @@ class Solution:
         cols = len(grid[0])
         queue = deque()
         fresh = 0
-
+        
+        # keep a track of rotten oranges and fresh oranges
+        # why keep a track of fresh oranges? 
+        # in case there are oranges which cannot be rottened by other oranges
+        
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 2:
@@ -14,16 +18,21 @@ class Solution:
 
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         levels = 0
-
+        
+        # for each rotten orange in queue
+        # we explore in 4 directions
+        #if explored cell is in bounds and has a fresh orange - change it to rotten,
+        # reduce count of fresh and append the new rotten coordinates to queue
+        # at the end, return levels - 1 if there are no fresh oranges left else -1
+        
         while queue:
             levels += 1
             for _ in range(len(queue)):
                 x, y = queue.popleft()
                 for dx, dy in directions:
                     r, c = x + dx, y + dy
-                    if r in range(rows) and c in range(cols) and grid[r][c] == 1:
+                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1:
                         fresh -= 1
                         grid[r][c] = 2
                         queue.append((r, c))
-
         return -1 if fresh != 0 else max(levels - 1, 0)
