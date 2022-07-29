d3f3cca87c1ec46e46bbb33b4af38f386c2d1890
Madhvi N <30744898+MayaScarlet@users.noreply.github.com>
2022-07-29 10:12:20 +0530

    Time: 58 ms (65.56%), Space: 14.2 MB (88.65%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -2,7 +2,14 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         dictionary = {}
         
+        # for every char in string s
         for index, char in enumerate(s):
+            
+            # if char doesn't exist in dictionary
+            # check if char at index in string t exists in dictionary values
+            # if yes, return false, else assign char at index in t to char
+            # if char exists in dictionary and char at index in t != dictionary[char]
+            # return false
             
             if char not in dictionary:
                 if t[index] in dictionary.values():
