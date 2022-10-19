7c2df24e75e559f8e37aef275b9668ff2479a8a4
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2022-10-19 22:26:08 +0530

    Time: 57 ms (50.24%), Space: 13.9 MB (68.81%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -1,20 +1,12 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
-#         counter = Counter(Counter(word))
-#         keys, values = counter.keys(), counter.values()
-
-#         if len(counter) == 1:
-#             return 1 in keys or 1 in values
+        counter = Counter(Counter(word).values())
+        keys, values = counter.keys(), counter.values()
         
-#         if len(counter) == 2:
-#             f1, f2 = min(keys), max(keys)
-#             return (f1 + 1 == f2 and counter[f2] == 1) or (f1 == 1 and counter[f1] == 1)
-#         return False
-    
-        cnt = Counter(Counter(word).values())
-        if (len(cnt) == 1):
-            return 1 in cnt.keys() or 1 in cnt.values()
-        if (len(cnt) == 2):
-            f1, f2 = min(cnt.keys()), max(cnt.keys())
-            return (f1 + 1 == f2 and cnt[f2] == 1) or (f1 == 1 and cnt[f1] == 1)
+        if len(counter) == 1:
+            return 1 in keys or 1 in values
+        
+        if len(counter) == 2:
+            f1, f2 = min(keys), max(keys)
+            return (f1 + 1 == f2 and counter[f2] == 1) or (f1 == 1 and counter[f1] == 1)
         return False
