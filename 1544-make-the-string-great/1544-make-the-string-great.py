90f503e61e12498b97248852b43790560d8175b9
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2022-11-08 13:43:54 +0530

    Time: 68 ms (53.21%), Space: 13.9 MB (15.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,17 +1,26 @@
 class Solution:
     def makeGood(self, s: str) -> str:
         
-        while len(s) > 1:
-            find = False
+#         while len(s) > 1:
+#             find = False
             
-            for i in range(len(s) - 1):
-                curr_char, next_char = s[i], s[i+1]
-                if abs(ord(curr_char) - ord(next_char)) == 32:
-                    s = s[:i] + s[i+2:]
+#             for i in range(len(s) - 1):
+#                 curr_char, next_char = s[i], s[i+1]
+#                 if abs(ord(curr_char) - ord(next_char)) == 32:
+#                     s = s[:i] + s[i+2:]
                     
-                    find = True
-                    break
-            if not find:
-                break
+#                     find = True
+#                     break
+#             if not find:
+#                 break
         
-        return s
+#         return s
+
+        stack = []
+        
+        for char in s:
+            if stack and abs(ord(char) - ord(stack[-1])) == 32:
+                stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
