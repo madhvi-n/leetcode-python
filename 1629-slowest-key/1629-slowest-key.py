076d2f9194674c21a841fded3f98e61e3102017f
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2022-11-08 18:01:11 +0530

    Time: 119 ms (53.29%), Space: 14.2 MB (8.16%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.py
@@ -0,0 +1,19 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        hashmap = dict()
+        hashmap[keysPressed[0]] = releaseTimes[0]
+        
+        for i in range(1, len(releaseTimes)):
+            current_duration = releaseTimes[i] - releaseTimes[i-1]
+            current_key = keysPressed[i]
+            hashmap[current_key] = max(current_duration, hashmap.get(current_key, 0))
+        
+        slowest_key = ''
+        longest_duration = 0
+        
+        for key, duration in hashmap.items():
+            if duration > longest_duration:
+                slowest_key, longest_duration = key, duration
+            elif duration == longest_duration and key > slowest_key:
+                slowest_key = key
+        return slowest_key
