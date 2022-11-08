6d18c5994b2619a58f421d74ab488f93fdf73b53
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2022-11-08 18:02:20 +0530

    Time: 60 ms (92.11%), Space: 14.1 MB (89.74%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -1,19 +1,30 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
-        hashmap = dict()
-        hashmap[keysPressed[0]] = releaseTimes[0]
+#         hashmap = dict()
+#         hashmap[keysPressed[0]] = releaseTimes[0]
         
-        for i in range(1, len(releaseTimes)):
-            current_duration = releaseTimes[i] - releaseTimes[i-1]
-            current_key = keysPressed[i]
-            hashmap[current_key] = max(current_duration, hashmap.get(current_key, 0))
+#         for i in range(1, len(releaseTimes)):
+#             current_duration = releaseTimes[i] - releaseTimes[i-1]
+#             current_key = keysPressed[i]
+#             hashmap[current_key] = max(current_duration, hashmap.get(current_key, 0))
         
-        slowest_key = ''
-        longest_duration = 0
+#         slowest_key = ''
+#         longest_duration = 0
         
-        for key, duration in hashmap.items():
-            if duration > longest_duration:
-                slowest_key, longest_duration = key, duration
-            elif duration == longest_duration and key > slowest_key:
-                slowest_key = key
-        return slowest_key
+#         for key, duration in hashmap.items():
+#             if duration > longest_duration:
+#                 slowest_key, longest_duration = key, duration
+#             elif duration == longest_duration and key > slowest_key:
+#                 slowest_key = key
+#         return slowest_key
+    
+        ansKey = keysPressed[0]
+        ansDuration = releaseTimes[0]
+        
+        for i in range(1, len(keysPressed)):
+            key = keysPressed[i]
+            duration = releaseTimes[i] - releaseTimes[i-1]
+            if duration > ansDuration or duration == ansDuration and key > ansKey:
+                ansKey = key
+                ansDuration = duration
+        return ansKey
