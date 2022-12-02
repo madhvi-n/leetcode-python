5d853f7c45f9360668ff355248b368e3fe4eaba9
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2022-12-02 10:18:47 +0530

    Time: 70 ms (44.22%), Space: 13.9 MB (77.84%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -3,15 +3,17 @@ class Solution:
         half = len(s) // 2
         vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         
-        pcount, qcount = 0, 0
+#         pcount, qcount = 0, 0
         
-        for i in range(half):
-            p, q = s[i], s[half + i]
+#         for i in range(half):
+#             p, q = s[i], s[half + i]
             
-            if p in vowels:
-                pcount += 1
+#             if p in vowels:
+#                 pcount += 1
                 
-            if q in vowels:
-                qcount += 1
+#             if q in vowels:
+#                 qcount += 1
                 
-        return pcount == qcount
+#         return pcount == qcount
+        n = len(s)
+        return sum(i in vowels for i in s[:n//2]) == sum(i in vowels for i in s[n//2:])
