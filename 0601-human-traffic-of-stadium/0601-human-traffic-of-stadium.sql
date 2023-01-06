71eaed376e2d27636e352f5abf64b86cb7120819
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-06 16:57:54 +0530

    Time: 309 ms (94.80%), Space: 0B (100.00%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+# select distinct t1.*
+# from stadium t1, stadium t2, stadium t3
+# where t1.people >= 100 and t2.people >= 100 and t3.people >= 100
+# and
+# (
+#     (t1.id - t2.id = 1 and t1.id - t3.id = 2 and t2.id - t3.id =1)  -- t1, t2, t3
+#     or
+#     (t2.id - t1.id = 1 and t2.id - t3.id = 2 and t1.id - t3.id =1) -- t2, t1, t3
+#     or
+#     (t3.id - t2.id = 1 and t2.id - t1.id =1 and t3.id - t1.id = 2) -- t3, t2, t1
+# )
+# order by t1.id
+# ;
+
+select id, visit_date, people
+from stadium
+where people>=100
+and
+((id+1 in (select id from stadium where people>=100) and id+2 in (select id from stadium where people>=100))
+OR (id+1 in (select id from stadium where people>=100) and id-1 in (select id from stadium where people>=100))
+OR (id-1 in (select id from stadium where people>=100) and id-2 in (select id from stadium where people>=100)))
+;
