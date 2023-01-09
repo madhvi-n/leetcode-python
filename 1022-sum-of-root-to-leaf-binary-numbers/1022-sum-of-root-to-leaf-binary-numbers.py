b014679a0556af2ed5ee79d33d0d8c7d5c1da6fe
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-09 19:28:28 +0530

    Time: 47 ms (63.57%), Space: 14.1 MB (79.76%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,18 +6,32 @@
 #         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        root_to_leaf = 0
-        stack = [(root, 0) ]
+#         root_to_leaf = 0
+#         stack = [(root, 0) ]
         
-        while stack:
-            root, curr_number = stack.pop()
-            if root is not None:
-                curr_number = (curr_number << 1) | root.val
-                # if it's a leaf, update root-to-leaf sum
+#         while stack:
+#             root, curr_number = stack.pop()
+#             if root is not None:
+#                 curr_number = (curr_number << 1) | root.val
+#                 # if it's a leaf, update root-to-leaf sum
                 
-                if root.left is None and root.right is None:
-                    root_to_leaf += curr_number
-                else:
-                    stack.append((root.right, curr_number))
-                    stack.append((root.left, curr_number))
-        return root_to_leaf
+#                 if root.left is None and root.right is None:
+#                     root_to_leaf += curr_number
+#                 else:
+#                     stack.append((root.right, curr_number))
+#                     stack.append((root.left, curr_number))
+#         return root_to_leaf
+
+        self.root_to_leaf = 0
+        def preorder(node, curr):
+            if node:
+                curr = curr << 1 | node.val
+                
+                if not node.left and not node.right:
+                    self.root_to_leaf += curr
+                
+                preorder(node.left, curr)
+                preorder(node.right, curr)
+        
+        preorder(root, 0)
+        return self.root_to_leaf
