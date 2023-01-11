df864780a10f9f399376ce5528dc93098291b040
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-11 12:32:09 +0530

    Time: 145 ms (79.96%), Space: 16.6 MB (58.63%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        seen = set()
         rows, cols = len(grid), len(grid[0])
         
         def calculate(row, col):
-            if row < 0 or col < 0 or row >= rows or col >= cols or (row, col) in seen or grid[row][col] == 0:
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] in [0, 2]:
                 return 0
             
-            seen.add((row, col))
+            grid[row][col] = 2
             
             subArea = calculate(row + 1, col) + calculate(row - 1, col) + \
                 calculate(row, col + 1) + calculate(row, col - 1)
@@ -17,6 +16,7 @@ class Solution:
         maxArea = 0
         for r in range(rows):
             for c in range(cols):
-                currentArea = calculate(r, c)
-                maxArea = max(maxArea, currentArea)
+                if grid[r][c]:
+                    currentArea = calculate(r, c)
+                    maxArea = max(maxArea, currentArea)
         return maxArea
