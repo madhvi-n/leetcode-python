f2f5d6a78e4620d4aef65dde8f45cc0d81b51a1f
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-11 15:32:52 +0530

    Time: 128 ms (90.76%), Space: 15.9 MB (6.84%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -7,6 +7,10 @@ class Solution:
         visited = set()
         
         def mark_closed_island(row, col):
+            # if row, col is out of bounds, return false and if current cell is 1, return True
+            # change the current cell to 1 and check for closed islands in all 4 directions recursively
+            # return top and bottom and left and right
+            
             if row < 0 or col < 0 or row >= rows or col >= cols:
                 return False
             
@@ -22,6 +26,9 @@ class Solution:
             right = mark_closed_island(row, col + 1)
             return top and bottom and left and right
         
+        # for each cell starting from 1st row to n - 1 rows and cols
+        # if current grid cell is 0 and not visited, check for closed islands
+        # if true, increment closed island count by 1
         closed_islands = 0
         for r in range(1, rows - 1):
             for c in range(1, cols - 1):
