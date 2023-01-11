c328dbf7677d4070443963fa1125a24d96d059f6
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-11 15:28:29 +0530

    Time: 131 ms (87.56%), Space: 16 MB (5.67%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,31 +1,32 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        if grid is None:
+        if not grid:
             return 0
-
-        rows = len(grid)
-        cols = len(grid[0])
-        closed_islands = 0
-        seen = set()
-
-        def is_closed_islands(row, col):
-            if row < 0 or row == rows or col < 0 or col == cols:
+        
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        
+        def mark_closed_island(row, col):
+            if row < 0 or col < 0 or row >= rows or col >= cols:
                 return False
-
+            
             if grid[row][col] == 1:
                 return True
-
+            
             grid[row][col] = 1
-            seen.add((row, col))
-            top = is_closed_islands(row - 1, col)
-            bottom = is_closed_islands(row + 1, col)
-            left = is_closed_islands(row, col - 1)
-            right = is_closed_islands(row, col + 1)
+            visited.add((row, col))
+            
+            top = mark_closed_island(row - 1, col)
+            bottom = mark_closed_island(row + 1, col)
+            left = mark_closed_island(row, col - 1)
+            right = mark_closed_island(row, col + 1)
             return top and bottom and left and right
-
-        for row in range(1, rows - 1):
-            for column in range(1, cols - 1):
-                if grid[row][column] == 0 and (row, column) not in seen:
-                    if is_closed_islands(row, column):
+        
+        closed_islands = 0
+        for r in range(1, rows - 1):
+            for c in range(1, cols - 1):
+                if grid[r][c] == 0 and (r, c) not in visited:
+                    if mark_closed_island(r, c):
                         closed_islands += 1
+        
         return closed_islands
