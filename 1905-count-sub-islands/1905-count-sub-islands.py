6870258a5e1c4ed70fc77e531296307a82a9755d
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-11 15:40:54 +0530

    Time: 2768 ms (99.41%), Space: 82.8 MB (59.60%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,25 +1,31 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         rows, cols = len(grid1), len(grid1[0])
-        sub_islands_count = 0
-
-        def dfs(row, col):
-            if row < 0 or col < 0 or row == rows or col == cols or grid2[row][col] == 0:
+        subIslands = 0
+        
+        def mark_sub_islands(row, col):
+            # if row, col is out of bounds or current cell in grid2 is 0, return True
+            # if current cell is both grid is 0, return False
+            # if current cell in both grid is 1, check for subislands recursively in 4 dirs
+            # return top and bottom and left and right
+            
+            if row < 0 or col < 0 or row >= rows or col >= cols or grid2[row][col] == 0:
                 return True
             
-            elif grid1[row][col] == 0 or grid2[row][col] == 0:
+            elif grid1[row][col] == 0 and grid2[row][col] == 0:
                 return False
-            
+             
             elif grid1[row][col] == 1 and grid2[row][col] == 1:
                 grid2[row][col] = 0
-                top = dfs(row + 1, col)
-                bottom = dfs(row - 1, col)
-                right = dfs(row, col + 1)
-                left = dfs(row, col - 1)
-            return top and bottom and left and right
+                top = mark_sub_islands(row - 1, col)
+                bottom = mark_sub_islands(row + 1, col)
+                left = mark_sub_islands(row, col - 1)
+                right = mark_sub_islands(row, col + 1)
+                return top and bottom and left and right
+            
         
         for r in range(rows):
             for c in range(cols):
-                if grid2[r][c] == 1 and dfs(r, c):
-                    sub_islands_count += 1
-        return sub_islands_count
+                if grid2[r][c] and mark_sub_islands(r, c):
+                    subIslands += 1
+        return subIslands
