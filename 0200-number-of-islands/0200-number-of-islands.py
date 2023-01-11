d4b2d3eefaa51057e6936233a1b2722e3cfa4aea
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-11 12:41:30 +0530

    Time: 272 ms (97.92%), Space: 16.4 MB (82.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,25 +6,26 @@ class Solution:
         
         rows, cols = len(grid), len(grid[0])
         
-        def dfs(row, col):
+        def mark_current_island(row, col):
             if row < 0 or col < 0 or row >= rows or col >= cols or grid[row][col] != "1":
                 return
             
             grid[row][col] = "2"
             
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
+            mark_current_island(row + 1, col)
+            mark_current_island(row - 1, col)
+            mark_current_island(row, col + 1)
+            mark_current_island(row, col - 1)
             
         islands = 0
         
+        # Check for every possible island for each cell of grid using dfs recursively
+        # for each island, increment the count, keep track of visited by adding it in visited set or marking it in place
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    mark_current_island(r, c)
                     islands += 1
-        
         return islands
             
             
