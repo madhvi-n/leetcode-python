30051fd0851178c64f19d1f2d06226084815ea4b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-11 09:28:03 +0530

    Time: 698 ms (86.54%), Space: 54.5 MB (33.33%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        adjacencyList = defaultdict(list)
+        
+        for src, dest in edges:
+            adjacencyList[src].append(dest)
+            adjacencyList[dest].append(src)
+        
+        visited = set()
+        
+        def dfs(node, parent):
+            total = 0
+            
+            if node not in visited:
+                visited.add(node)
+            
+                for child in adjacencyList[node]:
+                    if child != parent:
+                        childTime = dfs(child, node)
+                        
+                        if childTime > 0 or hasApple[child]:
+                            total += childTime + 2
+            return total
+        
+        return dfs(0, None)
