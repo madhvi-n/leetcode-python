1dd37c5563af89d9a6bb7bdd33ff984d098c8ca7
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-14 12:38:04 +0530

    Time: 51 ms (60.92%), Space: 14 MB (21.84%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def dfs(self, src, adjMatrix, visited, component):
+        pass
+        
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        unionFind = dict()
+        
+        def find(x):
+            unionFind.setdefault(x, x)
+            if x != unionFind[x]:
+                unionFind[x] = find(unionFind[x])
+            return unionFind[x]
+        
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            
+            
+            if rootX > rootY:
+                unionFind[rootX] = rootY
+            else:
+                unionFind[rootY] = rootX
+            
+            
+        
+        for i in range(len(s1)):
+            union(s1[i], s2[i])
+        
+        
+        res = []
+        
+        for c in baseStr:
+            res.append(find(c))
+        
+        return ''.join(res)
