a15ab6b8db37c84c4b4909b0e2b3da7be4b4b160
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-14 16:49:11 +0530

    Time: 99 ms (84.66%), Space: 14 MB (95.77%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,13 +1,35 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        n = len(nums)
+#         n = len(nums)
         
-        answer = 0
+#         answer = 0
         
-        for left in range(n):
-            min_val, max_val = float('inf'), float('-inf')
-            for right in range(left, n):
-                max_val = max(max_val, nums[right])
-                min_val = min(min_val, nums[right])
-                answer += max_val - min_val
+#         for left in range(n):
+#             min_val, max_val = float('inf'), float('-inf')
+#             for right in range(left, n):
+#                 max_val = max(max_val, nums[right])
+#                 min_val = min(min_val, nums[right])
+#                 answer += max_val - min_val
+#         return answer
+
+        n, answer = len(nums), 0 
+        stack = []
+        
+        # Find the sum of all the minimum.
+        for right in range(n + 1):
+            while stack and (right == n or nums[stack[-1]] >= nums[right]):
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                answer -= nums[mid] * (mid - left) * (right - mid)
+            stack.append(right)
+
+        # Find the sum of all the maximum.
+        stack.clear()
+        for right in range(n + 1):
+            while stack and (right == n or nums[stack[-1]] <= nums[right]):
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                answer += nums[mid] * (mid - left) * (right - mid)
+            stack.append(right)
+        
         return answer
