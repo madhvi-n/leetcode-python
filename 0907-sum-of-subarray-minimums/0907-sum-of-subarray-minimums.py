a42662d4177cae2f89bc8c28462501fb2368b8d5
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-14 16:21:47 +0530

    Time: 441 ms (99.36%), Space: 18.9 MB (59.83%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -17,31 +17,17 @@ class Solution:
         
 #         return minSum % mod
 
-        def get_previous_large_element(nums):
-            n = len(nums)
-            left, stack = [-1] * n, []
-            for i in range(n):
-                while stack and nums[i] < nums[stack[-1]]: 
-                    stack.pop()
-                left[i] = stack[-1] if stack else -1
-                stack.append(i)
-            for i in range(n):
-                left[i] =  i+1 if left[i] == -1 else i - left[i]
-            return left
-        
-        def get_next_large_element(nums):
-            n = len(nums)
-            right, stack = [-1] * n, []
-            for i in range(n):
-                while stack and nums[i] < nums[stack[-1]]: 
-                    right[stack.pop()] = i
-                stack.append(i)
-            for i in range(n):
-                right[i] =  n - i if right[i] == -1 else right[i] - i
-            return right
-        
-        mod = (10**9) + 7
-        left = get_previous_large_element(arr)
-        right = get_next_large_element(arr)
+        n = len(arr)
+        arr.append(0)
+        res = 0
+        mod = 1000000007
+        stack = [-1]
 
-        return sum(a*l*r for a, l, r in zip(arr, left, right)) % mod
+        for i, num in enumerate(arr):
+            while stack and arr[stack[-1]] > num: # (i)
+                current = stack.pop()
+                res += arr[current] * (i - current) * (current - stack[-1])
+
+            stack.append(i)
+
+        return res % (mod)
