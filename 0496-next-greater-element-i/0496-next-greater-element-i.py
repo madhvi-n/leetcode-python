c06e73b7006b47e0d5e657a730fb9da1e430f34a
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-14 14:22:10 +0530

    Time: 52 ms (80.98%), Space: 14.1 MB (54.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,33 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1_index = {num: index for index, num in enumerate(nums1)}
-        res = [-1] * len(nums1)
-        stack = []
+#         nums1_index = {num: index for index, num in enumerate(nums1)}
+#         res = [-1] * len(nums1)
+#         stack = []
 
+#         for i in range(len(nums2)):
+#             cur = nums2[i]
+#             while stack and cur > stack[-1]:
+#                 val = stack.pop()
+#                 idx = nums1_index[val]
+#                 res[idx] = cur
+#             if cur in nums1_index:
+#                 stack.append(cur)
+#         return res
+    
+        stack = []
+        hashmap = dict()
+        
         for i in range(len(nums2)):
-            cur = nums2[i]
-            while stack and cur > stack[-1]:
-                val = stack.pop()
-                idx = nums1_index[val]
-                res[idx] = cur
-            if cur in nums1_index:
-                stack.append(cur)
-        return res
+            hashmap[nums2[i]] = -1
+            
+        for i in range(len(nums2)):
+            while len(stack) > 0 and nums2[i] > stack[-1]:
+                curr = stack.pop()
+                hashmap[curr] = nums2[i]
+                
+            stack.append(nums2[i])
+            
+        for i in range(len(nums1)):
+            nums1[i] = hashmap[nums1[i]]
+        
+        return nums1
