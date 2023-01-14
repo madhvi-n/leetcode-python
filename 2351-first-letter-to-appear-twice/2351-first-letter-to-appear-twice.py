90e8efe36482f8eeda66ebd74f3fbde5bcb84522
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-14 20:33:01 +0530

    Time: 27 ms (94.45%), Space: 13.7 MB (94.84%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -3,9 +3,6 @@ class Solution:
         seen = set()
         
         for char in s:
-            if char not in seen:
-                seen.add(char)
-            else:
+            if char in seen:
                 return char
-        
-    
+            seen.add(char)
