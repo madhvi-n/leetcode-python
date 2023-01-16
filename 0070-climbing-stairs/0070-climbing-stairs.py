2ff2257e927ffb91281c934a472c14892d42dc7b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-16 20:15:15 +0530

    Time: 32 ms (80.90%), Space: 13.8 MB (54.97%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+#         one, two = 1, 1
+        
+#         for i in range(n - 1):
+#             temp = one
+#             one = one + two
+#             two = temp
+        
+#         return one
+        if n <= 2: 
+            return n
+
+        ways = [0 for _ in range(n)]
+        ways[0] = 1
+        ways[1] = 2
+
+        for i in range(2, n):
+            ways[i] += ways[i - 1] + ways[i - 2]
+
+        return ways[-1]
