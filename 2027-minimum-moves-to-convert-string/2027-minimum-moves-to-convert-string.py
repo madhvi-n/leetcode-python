a3c8988a140eb649891ee95f9a817683db662a8b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-17 10:47:10 +0530

    Time: 28 ms (93.79%), Space: 13.9 MB (54.83%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.py
@@ -1,5 +1,11 @@
 class Solution:
     def minimumMoves(self, s: str) -> int:
+        # initialize minMoves as 0
+        # scan string from left to right
+        # If char at current index is X, increment minMoves by 1 and increment index by 3 
+        # since window is 3 any X in this window will by flipped to 'O' in 1 count
+        # else increment index by 1
+        
         minMoves = i = 0
         while i < len(s): 
             if s[i] == "X": 
