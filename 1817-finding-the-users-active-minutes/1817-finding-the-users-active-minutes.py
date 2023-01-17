e876b312ae0c8a21626c60e50be028b1a8a7ae54
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-17 15:24:57 +0530

    Time: 981 ms (99.30%), Space: 25 MB (38.23%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+        hashmap = defaultdict(set)
+        
+        for user, time in logs:
+            hashmap[user].add(time)
+        
+        res = [0] * k
+        
+        for values in hashmap.values():
+            if len(values) <= k: 
+                res[len(values)-1] += 1
+        return res
