00f17ce0477b2829b46de2aada651753d254f7f4
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-18 13:59:29 +0530

    Time: 53 ms (93.23%), Space: 14.3 MB (26.56%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])
         
-        @lru_cache(None)
-        def dfs(row, col, product):
-            if not (0 <= row < rows) or not (0 <= col < cols):
-                return -1
-            
-            if row == rows - 1 and col == cols - 1:
-                return product * grid[row][col]
-            
-            return max(dfs(row + 1, col, product * grid[row][col]), dfs(row, col + 1, product * grid[row][col]))
+        @cache
+        def fn(i, j): 
+            """Return maximum & minimum products ending at (i, j)."""
+            if i == 0 and j == 0: return grid[0][0], grid[0][0]
+            if i < 0 or j < 0: return -inf, inf
+            if grid[i][j] == 0: return 0, 0
+            mx1, mn1 = fn(i-1, j) # from top
+            mx2, mn2 = fn(i, j-1) # from left 
+            mx, mn = max(mx1, mx2)*grid[i][j], min(mn1, mn2)*grid[i][j]
+            return (mx, mn) if grid[i][j] > 0 else (mn, mx)
         
-        max_product = dfs(0, 0, 1)
-        
-        return max_product % (10**9+7) if max_product != -1 else -1
+        mx, _ = fn(m-1, n-1)
+        return -1 if mx < 0 else mx % 1_000_000_007
