83b2d0b3594b3e9b6e39b16acfb793c6a5627d56
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-18 19:32:38 +0530

    Time: 35 ms (67.26%), Space: 13.8 MB (53.62%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -2,7 +2,11 @@ class Solution:
     def maxDepth(self, s: str) -> int:
         maxDepth = 0
         currDepth = 0
-
+        
+        # traverse through string
+        # for each open paranthesis, increment currentDepth and calculate maxDepth
+        # for each closed paranthesis, decrement currentDepth
+        
         for char in s:
             if char == '(':
                 currDepth += 1
