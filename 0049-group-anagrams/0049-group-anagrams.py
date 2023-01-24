6bc7124206f8c023326002222b3f12f11133d154
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-24 13:29:43 +0530

    Time: 133 ms (40.08%), Space: 18.5 MB (39.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,8 @@
 class Solution:
-    def groupAnagrams(self, words: List[str]) -> List[List[str]]:
-#         counter = defaultdict(list)
-#         for word in words:
-#             w = "".join(sorted(word))
-#             counter[w] += [word]
-
-#         result = []
-#         for values in counter.values():
-#             result.append(values)
-#         return result
-    
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
-        for word in words:
-            hashmap[tuple(sorted(word))].append(word)
-        return list(hashmap.values())
+        
+        for word in strs:
+            key = tuple(sorted(word))
+            hashmap[key].append(word)
+        return hashmap.values()
