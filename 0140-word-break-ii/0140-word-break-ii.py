b516edb982817b78ead95cc6397281b79ee4c596
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-24 13:06:50 +0530

    Time: 67 ms (5.29%), Space: 14 MB (29.67%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,39 @@
+class Trie:
+    def __init__(self):
+        self.root = {}
+        self.WORD_DELIM = '$'
+
+    def addWord(self, word):
+        cur = self.root
+        for char in word:
+            if char not in cur:
+                cur[char] = {}
+            cur = cur[char]
+        cur[self.WORD_DELIM] = word
+
+    def addWords(self, words):
+        for word in words:
+            self.addWord(word)
+
+    def getValidSentences(self, word, res = ''):            
+        res = []
+        def dfs(word=word, temp=[]):
+            cur = self.root
+            for i,char in enumerate(word):
+                if self.WORD_DELIM in cur:
+                    dfs(word[i:], temp + [cur[self.WORD_DELIM]])
+                if char not in cur:
+                    break
+                cur = cur[char]
+            else:
+                if self.WORD_DELIM in cur:
+                    res.append(' '.join(temp + [cur[self.WORD_DELIM]]))
+        dfs()
+        return res
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        trie = Trie()
+        trie.addWords(wordDict)
+        return trie.getValidSentences(s)
+        
