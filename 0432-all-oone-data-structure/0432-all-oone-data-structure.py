a673efc03e269f8777d65b5b923851d92f81d88e
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-24 14:03:59 +0530

    Time: 615 ms (24.49%), Space: 30.5 MB (55.04%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -2,42 +2,42 @@ class Block:
     def __init__(self, val=0):
         self.val = val
         self.keys = set()
-        self.before = None
-        self.after = None
+        self.prev = None
+        self.next = None
 
     def remove(self):
-        self.before.after = self.after
-        self.after.before = self.before
-        self.before, self.after = None, None
+        self.prev.next = self.next
+        self.next.prev = self.prev
+        self.prev, self.next = None, None
 
-    def insert_after(self, new_block):
-        old_after = self.after
-        self.after = new_block
-        new_block.before = self
-        new_block.after = old_after
-        old_after.before = new_block
+    def insert_next(self, new_block):
+        old_next = self.next
+        self.next = new_block
+        new_block.prev = self
+        new_block.next = old_next
+        old_next.prev = new_block
 
 
 class AllOne:
     def __init__(self):
-        self.begin = Block()  # dummy
-        self.end = Block()  # dummy
-        self.begin.after = self.end
-        self.end.before = self.begin
+        self.head = Block()  # dummy
+        self.tail = Block()  # dummy
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.mapping = {}  # key to block
 
     def inc(self, key):
         if not key in self.mapping:  # find current block and remove key
-            current_block = self.begin
+            current_block = self.head
         else:
             current_block = self.mapping[key]
             current_block.keys.remove(key)
 
-        if current_block.val + 1 != current_block.after.val:  # insert new block
+        if current_block.val + 1 != current_block.next.val:  # insert new block
             new_block = Block(current_block.val + 1)
-            current_block.insert_after(new_block)
+            current_block.insert_next(new_block)
         else:
-            new_block = current_block.after
+            new_block = current_block.next
 
         new_block.keys.add(key)  # update new_block
         self.mapping[key] = new_block  # ... and mapping of key to new_block
@@ -54,11 +54,11 @@ class AllOne:
         current_block.keys.remove(key)
 
         if current_block.val != 1:
-            if current_block.val - 1 != current_block.before.val:  # insert new block
+            if current_block.val - 1 != current_block.prev.val:  # insert new block
                 new_block = Block(current_block.val - 1)
-                current_block.before.insert_after(new_block)
+                current_block.prev.insert_next(new_block)
             else:
-                new_block = current_block.before
+                new_block = current_block.prev
             new_block.keys.add(key)
             self.mapping[key] = new_block
 
@@ -66,17 +66,17 @@ class AllOne:
             current_block.remove()
 
     def getMaxKey(self):
-        if self.end.before.val == 0:
+        if self.tail.prev.val == 0:
             return ""
-        key = self.end.before.keys.pop()  # pop and add back to get arbitrary (but not random) element
-        self.end.before.keys.add(key)
+        key = self.tail.prev.keys.pop()  # pop and add back to get arbitrary (but not random) element
+        self.tail.prev.keys.add(key)
         return key
 
     def getMinKey(self):
-        if self.begin.after.val == 0:
+        if self.head.next.val == 0:
             return ""
-        key = self.begin.after.keys.pop()
-        self.begin.after.keys.add(key)
+        key = self.head.next.keys.pop()
+        self.head.next.keys.add(key)
         return key
 
 # Your AllOne object will be instantiated and called as such:
