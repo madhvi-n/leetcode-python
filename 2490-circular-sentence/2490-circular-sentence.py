5fc3d0aad9cd9790e205a089d46c77c304dab923
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-25 13:22:45 +0530

    Time: 27 ms (97.39%), Space: 13.7 MB (97.02%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.py
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -5,9 +5,12 @@ class Solution:
         
         sentence = sentence.split()
         
+        # for each word, check next word
+        # if current's last is not equal to next's first character, return False
+        
         for i in range(len(sentence)):
-            curr = sentence[i]
-            nxt = sentence[i + 1] if (i + 1) < len(sentence) else sentence[0]
+            curr = sentence[i - 1]
+            nxt = sentence[i]
             
             if curr[-1] != nxt[0]:
                 return False
