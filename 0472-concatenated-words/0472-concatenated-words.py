f8f22ea0f828ca709ea0dfe020dd325e40b37650
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-27 12:10:50 +0530

    Time: 617 ms (50.97%), Space: 33.6 MB (12.38%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        dictionary = set(words)
+        memo = {}
+        
+        words.sort(key = lambda x: len(x))
+        
+        def dfs(word):
+            if word in memo:
+                return memo[word]
+            
+            memo[word] = False
+            
+            for i in range(1, len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+                
+                if prefix in dictionary and suffix in dictionary:
+                    memo[word] = True
+                    break
+                    
+                if prefix in dictionary and dfs(suffix):
+                    memo[word] = True
+                    break
+                    
+                if suffix in dictionary and dfs(prefix):
+                    memo[word] = True
+                    break
+                    
+            return memo[word]
+        return [word for word in words if dfs(word)]
