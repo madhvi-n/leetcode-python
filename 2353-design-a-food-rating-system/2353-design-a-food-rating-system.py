316781834b3c4dd3df949af5d5553b537f1a1756
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-29 12:58:54 +0530

    Time: 908 ms (66.03%), Space: 45.2 MB (51.92%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -0,0 +1,25 @@
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.mapping = {}
+        self.ratings = defaultdict(list)
+        for food, cuisine, rating in zip(foods, cuisines, ratings): 
+            self.mapping[food] = (cuisine, rating)
+            heapq.heappush(self.ratings[cuisine],(-rating, food))
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine, rating = self.mapping[food]
+        self.mapping[food] = cuisine, newRating
+        heapq.heappush(self.ratings[cuisine],(-newRating, food))
+
+    def highestRated(self, cuisine: str) -> str:
+        while self.ratings[cuisine] and -self.ratings[cuisine][0][0] != self.mapping[self.ratings[cuisine][0][1]][1]:
+            heapq.heappop(self.ratings[cuisine])
+        return self.ratings[cuisine][0][1]
+        
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
