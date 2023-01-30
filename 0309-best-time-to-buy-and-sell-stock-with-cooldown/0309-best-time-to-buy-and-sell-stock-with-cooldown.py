6a94c2eb5771fa422aa18c27c4664f608807ad6d
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-30 20:51:42 +0530

    Time: 49 ms (64.70%), Space: 18.3 MB (21.20%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,24 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # State: Buying or Selling?
-        # If Buy -> i + 1
-        # If Sell -> i + 2
-
-        dp = {}  # key=(i, buying) val=max_profit
-
-        def dfs(i, buying):
-            if i >= len(prices):
-                return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-
-            cooldown = dfs(i + 1, buying)
-            if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
-            else:
-                sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-            return dp[(i, buying)]
-
-        return dfs(0, True)
+        @lru_cache(None)
+        def recurse(i, stock):
+            if i >= n: return 0
+            
+            buy = (-prices[i] + recurse(i+1, 1)) if stock == 0 else -inf
+            sell = (prices[i] + recurse(i+2, 0)) if stock == 1 else -inf
+            hold = recurse(i+1, stock)
+            
+            return max(buy, sell, hold)
+            
+        n = len(prices)
+        return recurse(0, 0)
