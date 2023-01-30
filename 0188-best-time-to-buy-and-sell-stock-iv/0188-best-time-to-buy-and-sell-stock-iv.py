acf1adf6d93f3cef7364a2578a2699c2846539ad
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-30 20:55:54 +0530

    Time: 417 ms (26.57%), Space: 89.5 MB (5.09%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        @lru_cache(None)
+        def recurse(i, stock, count):
+            if i >= n: return 0
+            if count >= k: return 0
+            
+            buy = (-prices[i] + recurse(i+1, 1, count)) if stock == 0 else -inf
+            sell = (prices[i] + recurse(i+1, 0, count+1)) if stock == 1 else -inf
+            hold = recurse(i+1, stock, count)
+            
+            return max(buy, sell, hold)
+            
+        n = len(prices)
+        return recurse(0, 0, 0)
