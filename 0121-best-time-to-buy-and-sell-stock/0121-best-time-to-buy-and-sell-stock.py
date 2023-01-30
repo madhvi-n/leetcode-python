cd9fe86a3f2af8ce9527f3ea9194d44b062bd848
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-30 20:59:30 +0530

    Time: 1031 ms (82.87%), Space: 25 MB (35.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @lru_cache(None)
-        def recurse(i, stock, count):
-            if i >= n: return 0
-            if count >= k: return 0
-            
-            buy = (-prices[i] + recurse(i+1, 1, count)) if stock == 0 else -inf
-            sell = (prices[i] + recurse(i+1, 0, count+1)) if stock == 1 else -inf
-            hold = recurse(i+1, stock, count)
-            
-            return max(buy, sell, hold)
-            
         n = len(prices)
-        k = 1 # set number of transactions here
-        return recurse(0, 0, 0)
+        min_price, max_profit = float('inf'), 0
+
+        for i in range(n):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                max_profit = max(max_profit, prices[i] - min_price)
+
+        return max_profit
