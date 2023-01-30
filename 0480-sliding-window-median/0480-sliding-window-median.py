81689223bdf0ce7710d762100b38c5934e1ddbc8
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-30 13:29:16 +0530

    Time: 452 ms (41.96%), Space: 15.7 MB (95.62%) - LeetHub

---
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -0,0 +1,16 @@
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        d = deque()
+        result = []
+        
+        for i, n in enumerate(nums):
+            if i >= k:
+                d.remove(nums[i-k])
+            insort_left(d, n)
+            if i >= k-1:
+                if k % 2 == 0:
+                    m = (d[k//2 - 1] + d[k//2]) / 2
+                else:
+                    m = d[k//2]
+                result.append(m)
+        return result
