b75457cf1f77372319e24ef77a9475d678b2b9c9
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-01-31 16:21:30 +0530

    Time: 107 ms (77.52%), Space: 19.1 MB (8.43%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -8,18 +8,37 @@ from queue import PriorityQueue
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        nodes = []
-        head = point = ListNode(0)
+        # O(nlogn)
+#         nodes = []
+#         head = point = ListNode(0)
         
-        for curr_list in lists:
-            while curr_list:
-                nodes.append(curr_list.val)
-                curr_list = curr_list.next
+#         for curr_list in lists:
+#             while curr_list:
+#                 nodes.append(curr_list.val)
+#                 curr_list = curr_list.next
         
-        nodes.sort()
+#         nodes.sort()
         
-        for x in nodes:
-            point.next = ListNode(x)
-            point = point.next
+#         for x in nodes:
+#             point.next = ListNode(x)
+#             point = point.next
 
-        return head.next
+#         return head.next
+
+        dummy = curr = ListNode(0)
+        heap = []
+        
+        for ind, el in enumerate(lists):
+            if el: 
+                heappush(heap, (el.val, ind))
+                
+        while heap:
+            val, ind = heappop(heap)
+            curr.next = ListNode(val)
+            curr = curr.next
+            
+            if lists[ind].next:
+                lists[ind] = lists[ind].next
+                heappush(heap, (lists[ind].val, ind))
+                
+        return dummy.next
