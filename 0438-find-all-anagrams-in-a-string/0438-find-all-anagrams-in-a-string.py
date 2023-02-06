c1502bbc54408a3a2ca180add2d8df5e527bb2ac
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-06 12:33:33 +0530

    Time: 111 ms (85.34%), Space: 15.2 MB (26.74%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(p) > len(s):
+            return []
+        
+        pCount, sCount = {}, {}
+        left = 0
+        
+        for i in range(len(p)):
+            pCount[p[i]] = 1 + pCount.get(p[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+        
+        res = [0] if pCount == sCount else []
+        
+        for right in range(len(p), len(s)):
+            sCount[s[right]] = 1 + sCount.get(s[right], 0)
+            sCount[s[left]] -= 1
+            
+            if sCount[s[left]] == 0:
+                sCount.pop(s[left])
+            
+            left += 1
+            
+            if pCount == sCount:
+                res.append(left)
+        return res
