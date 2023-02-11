4b8c03cb8103d9358990ad0744fc4ab5b6ab1382
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-11 21:19:37 +0530

    Time: 198 ms (67.41%), Space: 14.7 MB (71.38%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -5,10 +5,15 @@ class Solution:
         max_vowels = 0
         curr_vowels = 0
         
+        # traverse through string
+        # for each index, if current char is vowel, increment count
+        # if index exceed window, check if last character in prev window is vowel
+        # if yes, reduce count of curr_vowel
+        # for each iteration, keep track of max vowels
+        
         for i in range(len(s)):
             if i >= k and s[i - k] in vowels:
                 curr_vowels -= 1
-                
             if s[i] in vowels:
                 curr_vowels += 1
             max_vowels = max(max_vowels, curr_vowels)
