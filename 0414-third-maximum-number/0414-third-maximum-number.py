d8450f48d15fb8431beeaca5c626ea263872230e
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-12 15:43:11 +0530

    Time: 65 ms (28.70%), Space: 15.5 MB (48.43%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = set(nums)
+        
+        heap = []
+        
+        for num in nums:
+            if len(heap) == 3:
+                if heap[-1] < num:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, num)
+            else:
+                heapq.heappush(heap, num)
+        
+        if len(heap) == 1:
+            return heap[0]
+        
+        elif len(heap) == 2:
+            first_num = heap[0]
+            heappop(heap)
+            return max(first_num, heap[0])
+        
+        return heap[0]
