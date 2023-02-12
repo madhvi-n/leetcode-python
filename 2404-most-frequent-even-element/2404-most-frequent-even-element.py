0178aef2cdeaf602a05c1526ace83280d719172b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-12 14:09:54 +0530

    Time: 303 ms (55.69%), Space: 14.4 MB (48.82%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -1,5 +1,6 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
+        # Keep track of frequency of even numbers and max frequency 
         freq = dict()
         maxlen = 0
         for num in nums:
@@ -7,8 +8,13 @@ class Solution:
                 freq[num] = 1 + freq.get(num, 0)
                 maxlen = max(maxlen, freq.get(num))
         
+        # if dict is empty, return -1 since all numbers are odd
         if len(freq) == 0:
             return -1
+        
+        # Iterate through frequency
+        # Push the key if values equals maxlen
+        # Create a monotonic stack in ascending order
         
         stack = []
         for key, val in freq.items():
