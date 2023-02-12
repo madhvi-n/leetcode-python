34990b1701fb74738bccbda162c11f6a9e2cd5f5
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-12 13:15:48 +0530

    Time: 110 ms (61.03%), Space: 18.6 MB (89.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -36,9 +36,11 @@ class Solution:
             heappush(h, (d[key], key)) # freq, item - O(log(k))
             if len(h) > k:
                 heappop(h)
-
-        res = []
-        while h: # O(k)
-            frq, item = heappop(h) # O(logk)
-            res.append(item)
-        return res
+    
+        return [val for (key, val) in h]
+        
+        # res = []
+        # while h: # O(k)
+        #     frq, item = heappop(h) # O(logk)
+        #     res.append(item)
+        # return res
