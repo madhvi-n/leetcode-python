8350da7e83133d7f5479b3d210117311099f8e63
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-12 12:56:30 +0530

    Time: 101 ms (88.37%), Space: 18.6 MB (67.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,44 @@
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = dict()
-        freq = [[] for i in range(len(nums) + 1)]
-
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
+#         count = dict()
+#         frequency = defaultdict(list)
+        
+#         for num in nums:
+#             count[num] = 1 + count.get(num, 0)
             
-        for n, c in count.items():
-            freq[c].append(n)
+#         for n, c in count.items():
+#             frequency[c].append(n)
+        
+#         res = []
+        
+#         for i in range(len(frequency) - 1, 0, -1):
+#             for n in frequency[i]:
+#                 res.append(n)
+                
+#                 if len(res) == k:
+#                     return res
+        
+#         if len(nums) == 1:
+#             return [nums[0]]
+
+        # freq dict
+        d = defaultdict(int)
+        
+        for num in nums:
+            d[num] += 1
+
+        # insert k items into heap O(nlog(k))
+        h = []
+        from heapq import heappush, heappop
+        for key in d: # O(N)
+            heappush(h, (d[key], key)) # freq, item - O(log(k))
+            if len(h) > k:
+                heappop(h)
 
         res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        while h: # O(k)
+            frq, item = heappop(h) # O(logk)
+            res.append(item)
+        return res
