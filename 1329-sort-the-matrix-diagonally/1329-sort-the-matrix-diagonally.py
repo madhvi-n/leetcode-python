99f87dc65786b54a31bcf7d4a41b975ae813b281
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-12 18:06:24 +0530

    Time: 82 ms (87.63%), Space: 14.4 MB (16.44%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,16 +1,24 @@
 class Solution:
-    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(mat), len(mat[0])
-        hashmap = defaultdict(list)
-        
-        for i in range(rows):
-            for j in range(cols):
-                hashmap[i - j].append(mat[i][j])
-        
-        for k in hashmap:
-            hashmap[k].sort(reverse=True)
-        
-        for i in range(rows):
-            for j in range(cols):
-                mat[i][j] = hashmap[i - j].pop()
-        return mat
+    def diagonalSort(self, matrix: List[List[int]]) -> List[List[int]]:
+        # Step 1: Create a dictionary to store the diagonals
+        diagonals = {}
+
+        # Step 2: Traverse the matrix row by row and add each element to the corresponding diagonal
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                key = i - j
+                if key not in diagonals:
+                    diagonals[key] = []
+                diagonals[key].append(matrix[i][j])
+
+        # Step 3: Sort each diagonal in the dictionary
+        for key in diagonals:
+            diagonals[key].sort()
+
+        # Step 4: Traverse the matrix row by row again and replace each element with the next element in the corresponding diagonal
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                key = i - j
+                matrix[i][j] = diagonals[key].pop(0)
+
+        return matrix
