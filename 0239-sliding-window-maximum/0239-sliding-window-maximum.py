f6b84fa9f413fbdb711f07016742e3ced445a6a2
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-14 12:55:05 +0530

    Time: 1777 ms (70.67%), Space: 30.1 MB (45.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,25 +1,23 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        """
-        Initialize an empty deque q to store the indices of elements in the window.
-        Loop through the input list of numbers arr with an index i.
-        In each iteration, pop elements from the deque q until the deque is empty or the value of the last element in the deque is less than or equal to the current element arr[i].
-        Append the index i to the deque q.
-        If the first element in the deque q is equal to i - k, then pop it from the deque, since it is outside the current window.
-        If i is greater than or equal to k - 1, append the value arr[q[0]] to the result list, which represents the maximum value in the current window.
-        Return the result list after the loop.
-        """
         result = []
-        q = deque()
         
+        # Initialize an empty deque q to store the indices of elements in the window.
+        queue = deque()
+        
+        # Loop through the input list of numbers arr with an index i.
+        # In each iteration, pop elements from the deque q until the deque is empty or the value of the last element in the deque is less than or equal to the current element arr[i].
+        # Append the index i to the deque q.
         for i in range(len(nums)):
-            while q and nums[q[-1]] <= nums[i]:
-                q.pop()
-            q.append(i)
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            queue.append(i)
             
-            if q[0] == i - k:
-                q.popleft()
+            # If the first element in the deque q is equal to i - k, then pop it from the deque, since it is outside the current window.
+            if queue[0] == i - k:
+                queue.popleft()
             
+            # If i is greater than or equal to k - 1, append the value arr[q[0]] to the result list, which represents the maximum value in the current window.
             if i >= k - 1:
-                result.append(nums[q[0]])
+                result.append(nums[queue[0]])
         return result
