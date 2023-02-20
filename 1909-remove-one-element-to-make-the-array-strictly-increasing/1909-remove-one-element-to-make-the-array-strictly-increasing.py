efd6a78c824708e8f27b3b14124106277912a453
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-02-20 11:45:47 +0530

    Time: 61 ms (54.38%), Space: 14 MB (41.72%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canBeIncreasing(self, nums: List[int]) -> bool:
+        prev, seen = float('-inf'), False
+        
+        for i, x in enumerate(nums): 
+            if prev < x: prev = x
+            else: 
+                if seen: 
+                    return False 
+                seen = True 
+                if i == 1 or nums[i-2] < x: 
+                    prev = x
+        return True
