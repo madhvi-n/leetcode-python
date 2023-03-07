91cedf15b2b20cef6ca5a21c541683c59f16f710
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-03-07 17:57:14 +0530

    Time: 806 ms (38.59%), Space: 28.5 MB (67.27%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -1,45 +1,46 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         n = len(arr)
-        
         if n <= 1:
             return 0
-        
-        graph = defaultdict(list)
-        
+
+        graph = {}
         for i in range(n):
-            graph[arr[i]].append(i)
-        
-        
-        queue = [0]
-        visited = set([0])
+            if arr[i] in graph:
+                graph[arr[i]].append(i)
+            else:
+                graph[arr[i]] = [i]
+
+        curs = [0]  # store current layers
+        visited = {0}
         step = 0
-        
-        while queue:
-            next_level = []
-            
+
+        # when current layer exists
+        while curs:
+            nex = []
+
             # iterate the layer
-            for node in queue:
-                # check if node is reached
-                if node == n - 1:
+            for node in curs:
+                # check if reached end
+                if node == n-1:
                     return step
-                
-                # check child nodes for curr node
+
+                # check same value
                 for child in graph[arr[node]]:
                     if child not in visited:
                         visited.add(child)
-                        next_level.append(child)
-                        
+                        nex.append(child)
+
                 # clear the list to prevent redundant search
                 graph[arr[node]].clear()
-                
-                # check neighbors of curr node
-                for child in [node - 1, node + 1]:
-                    if 0 <= child < n and child not in visited:
+
+                # check neighbors
+                for child in [node-1, node+1]:
+                    if 0 <= child < len(arr) and child not in visited:
                         visited.add(child)
-                        next_level.append(child)
-                
-            queue = next_level
+                        nex.append(child)
+
+            curs = nex
             step += 1
-        
+
         return -1
