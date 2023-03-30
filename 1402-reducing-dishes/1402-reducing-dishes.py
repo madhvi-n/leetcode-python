ed6060425346497c37a0608268d2fb1d661d3cab
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-03-30 10:44:07 +0530

    Time: 124 ms (38.63%), Space: 14 MB (46.11%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,8 +1,23 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        res = total = 0
         satisfaction.sort()
-        while satisfaction and satisfaction[-1] + total > 0:
-            total += satisfaction.pop()
-            res += total
-        return res
+        
+        n = len(satisfaction)
+        
+        if satisfaction[-1] <= 0:
+            return 0
+        
+        ans = float('-inf')
+        
+        for i in range(n):
+            curr = 0
+            
+            for j in range(i, n):
+                curr += (satisfaction[j] * (j - i + 1))
+            
+            ans = max(curr, ans)
+            
+            if satisfaction[i] >= 0:
+                break
+        
+        return ans
