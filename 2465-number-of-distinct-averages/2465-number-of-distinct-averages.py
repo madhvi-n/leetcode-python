7053cbc6e231c3367e3db5ee7a9b530e596aa62b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-06 15:07:06 +0530

    Time: 28 ms (90.93%), Space: 13.8 MB (45.04%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.py
@@ -4,13 +4,17 @@ class Solution:
         
         nums.sort()
         
-        left, right = 0, len(nums) - 1
+#         left, right = 0, len(nums) - 1
         
-        while left < right:
-            avg = (nums[left] + nums[right]) / 2
-            result.add(avg)
+#         while left < right:
+#             avg = (nums[left] + nums[right]) / 2
+#             result.add(avg)
             
-            left += 1
-            right -= 1
+#             left += 1
+#             right -= 1
         
+#         return len(result)
+    
+        for i in range(len(nums)//2):
+            result.add((nums[i] + nums[len(nums) - i -1]) / 2)
         return len(result)
