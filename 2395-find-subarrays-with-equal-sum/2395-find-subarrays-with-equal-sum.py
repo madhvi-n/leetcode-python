c487b6f056b633fd9d6643bec24e16277566aa0b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-06 11:19:34 +0530

    Time: 35 ms (81.11%), Space: 13.9 MB (72.56%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
@@ -1,11 +1,19 @@
 class Solution:
     def findSubarrays(self, nums: List[int]) -> bool:
-        hashmap = defaultdict(list)
+#         hashmap = defaultdict(list)
         
+#         for i in range(len(nums) - 1):
+#             hashmap[(nums[i] + nums[i+1])].append((nums[i], nums[i+1]))
+        
+#         for subarraySum, values in hashmap.items():
+#             if len(values) >= 2:
+#                 return True
+#         return False
+
+        subarraySum = set()
         for i in range(len(nums) - 1):
-            hashmap[(nums[i] + nums[i+1])].append((nums[i], nums[i+1]))
-        
-        for subarraySum, values in hashmap.items():
-            if len(values) >= 2:
+            currSum = nums[i] + nums[i+1]
+            if currSum in subarraySum:
                 return True
+            subarraySum.add(currSum)
         return False
