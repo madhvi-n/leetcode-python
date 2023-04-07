5a70318184a53bf0db7ecaefc1a924c2cb9b5534
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-07 17:18:20 +0530

    Time: 828 ms (32.19%), Space: 109.1 MB (5.01%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -2,25 +2,23 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         visited = set()
-
-        # return no of lands cells
+        
         def dfs(row, col):
-            if row < 0 or col < 0 or row == ROWS or col == COLS or (row, col) in visited or not grid[row][col]:
+            if row < 0 or col < 0 or row == ROWS or col == COLS or not grid[row][col] or (row, col) in visited:
                 return 0
             visited.add((row, col))
-            res = 1 # we are at land cell and now look in 4 directions for possible land
-            directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-            for dx, dy in directions:
-                res += dfs(row + dx, col + dy)
+            res = 1
+            for dx, dy in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                res += dfs(dx, dy)
             return res
-
+        
         land, borderLand = 0, 0
-
+        
         for r in range(ROWS):
             for c in range(COLS):
                 land += grid[r][c]
-                # checking grid is 1 and not visited and lies on the edge of the grid/perimeter
-                if grid[r][c] and (r, c) not in visited and (c in [0, COLS - 1] or r in [0, ROWS - 1]):
+                
+                if grid[r][c] == 1 and (r, c) not in visited and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
                     borderLand += dfs(r, c)
-
+        
         return land - borderLand
