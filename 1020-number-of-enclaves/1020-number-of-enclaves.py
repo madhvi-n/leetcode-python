ab360f9d0c80f8896a1f84d064e1ee9354c0a952
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-07 12:07:02 +0530

    Time: 755 ms (43.48%), Space: 96.1 MB (16.88%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,44 +1,26 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
+        ROWS, COLS = len(grid), len(grid[0])
         visited = set()
-        visited = [[False] * cols for _ in range(rows)]
-        
+
+        # return no of lands cells
         def dfs(row, col):
-            # if row or col is out of bounds or current cell in grid is 0 or visited, return
-            if row < 0 or row >= rows or col < 0 or col >= cols or\
-                grid[row][col] == 0 or visited[row][col]:
-                return
-            
-            # mark current cell as visited and traverse in 4 directions
-            visited[row][col] = True
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-            
-        
-        for r in range(rows):
-            # first column
-            if grid[r][0] == 1 and not visited[r][0]:
-                dfs(r, 0)
-                
-            # last column
-            if grid[r][cols - 1] == 1 and not visited[r][cols - 1]:
-                dfs(r, cols - 1)
-        
-        for c in range(cols):
-            # first row
-            if grid[0][c] == 1 and not visited[0][c]:
-                dfs(0, c)
-            
-            # last row
-            if grid[rows - 1][c] == 1 and not visited[rows - 1][c]:
-                dfs(rows - 1, c)
-        
-        count = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1 and not visited[r][c]:
-                    count += 1
-        return count
+            if row < 0 or col < 0 or row == ROWS or col == COLS or (row, col) in visited or not grid[row][col]:
+                return 0
+            visited.add((row, col))
+            res = 1 # we are at land cell and now look in 4 directions for possible land
+            directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+            for dx, dy in directions:
+                res += dfs(row + dx, col + dy)
+            return res
+
+        land, borderLand = 0, 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                land += grid[r][c]
+                # checking grid is 1 and not visited and lies on the edge of the grid/perimeter
+                if grid[r][c] and (r, c) not in visited and (c in [0, COLS - 1] or r in [0, ROWS - 1]):
+                    borderLand += dfs(r, c)
+
+        return land - borderLand
