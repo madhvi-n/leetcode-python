c0974a7a9c6ca1940cefc1c9983461a317fd9d4d
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-07 10:40:04 +0530

    Time: 627 ms (89.11%), Space: 75.8 MB (59.47%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -2,41 +2,43 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+        visited = [[False] * cols for _ in range(rows)]
         
         def dfs(row, col):
-            if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == 0 or (row, col) in visited:
+            # if row or col is out of bounds or current cell in grid is 0 or visited, return
+            if row < 0 or row >= rows or col < 0 or col >= cols or\
+                grid[row][col] == 0 or visited[row][col]:
                 return
             
-            visited.add((row, col))
-            for dx, dy in directions:
-                new_row, new_col = row + dx, col + dy
-                dfs(new_row, new_col)
+            # mark current cell as visited and traverse in 4 directions
+            visited[row][col] = True
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
             
         
         for r in range(rows):
             # first column
-            if grid[r][0] == 1 and (r, 0) not in visited:
+            if grid[r][0] == 1 and not visited[r][0]:
                 dfs(r, 0)
                 
             # last column
-            if grid[r][cols - 1] == 1 and (r, cols - 1) not in visited:
+            if grid[r][cols - 1] == 1 and not visited[r][cols - 1]:
                 dfs(r, cols - 1)
         
         for c in range(cols):
             # first row
-            if grid[0][c] == 1 and (0, c) not in visited:
+            if grid[0][c] == 1 and not visited[0][c]:
                 dfs(0, c)
             
             # last row
-            if grid[rows - 1][c] == 1 and (rows - 1, c) not in visited:
+            if grid[rows - 1][c] == 1 and not visited[rows - 1][c]:
                 dfs(rows - 1, c)
         
         count = 0
-        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and (r, c) not in visited:
+                if grid[r][c] == 1 and not visited[r][c]:
                     count += 1
-        
         return count
