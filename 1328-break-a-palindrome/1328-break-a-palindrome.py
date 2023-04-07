dd90f37720ffb69b018590054cf6654fa083b776
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-07 14:09:28 +0530

    Time: 38 ms (14.41%), Space: 13.9 MB (8.43%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,5 +1,11 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
+        """
+        Start by iterating through the string from left to right until we find a non-'a' character. 
+        We can then replace that character with 'a' and return the modified string.
+        If we reach the end of the string without finding a non-'a' character, we can simply replace 
+        the last character with 'b' to get the lexicographically smallest string that is not a palindrome.
+        """
         n = len(palindrome)
         for i in range(n // 2):
             if palindrome[i] != 'a':
