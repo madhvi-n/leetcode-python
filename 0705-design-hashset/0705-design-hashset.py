800c931ecb812d0334cf2ea2dbdb708f83fdf236
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-08 13:10:47 +0530

    Time: 242 ms (49.05%), Space: 19.3 MB (42.96%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,19 +1,47 @@
+class ListNode:
+    def __init__(self, key=-1, next_node=None):
+        self.key = key
+        self.next = next_node
+        
 class MyHashSet:
 
     def __init__(self):
-        self.stack = []
-
+        self.MOD = 1009
+        self.map = [ListNode() for _ in range(self.MOD + 1)]
+    
+    def hashkey(self, key):
+        FNV_OFFSET = 14695981039346656037
+        FNV_PRIME = 1099511628211
+        
+        hash_val = FNV_OFFSET
+        key_bytes = str(key).encode('utf-8')
+        for byte in key_bytes:
+            hash_val = (hash_val ^ byte) * FNV_PRIME
+        return hash_val % self.MOD
+    
     def add(self, key: int) -> None:
-        if key not in self.stack:
-            self.stack.append(key)
-
+        curr = self.map[self.hashkey(key)]
+        while curr.next:
+            if curr.next.key == key:
+                return
+            curr = curr.next
+        curr.next = ListNode(key)
+            
     def remove(self, key: int) -> None:
-        if key in self.stack:
-            self.stack.remove(key)
+        curr = self.map[self.hashkey(key)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
 
     def contains(self, key: int) -> bool:
-        return key in self.stack
-
+        curr = self.map[self.hashkey(key)]
+        while curr.next:
+            if curr.next.key == key:
+                return True
+            curr = curr.next
+        return False
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
