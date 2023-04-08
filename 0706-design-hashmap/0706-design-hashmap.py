1c547190f8b73749098f025640895109aed3ad16
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-08 12:33:29 +0530

    Time: 257 ms (60.14%), Space: 17.5 MB (46.91%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,49 +1,46 @@
+class ListNode:
+    def __init__(self, key=-1, val=-1, next_node=None):
+        self.val = val
+        self.key = key
+        self.next = next_node
+        
 class MyHashMap:
 
     def __init__(self):
-        self.size = 2069 #use size (a prime) to generate hashkey
-        self.buckets = defaultdict(list)
+        self.MOD = 1009
+        self.hashmap = [ListNode() for _ in range(self.MOD + 1)]
         
-
+    def hashkey(self, key) -> int:
+        return key % self.MOD
+    
     def put(self, key: int, value: int) -> None:
-        bucket, index = self.getIndex(key)
-        if index < 0:
-            bucket.append((key,value))
-        else:
-            bucket[index] = (key, value)
-
+        index = self.hashkey(key)
+        curr = self.hashmap[index]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.val = value
+                return
+            curr = curr.next
+        curr.next = ListNode(key, value)
+        
     def get(self, key: int) -> int:
-        bucket, index = self.getIndex(key)
-        if index < 0:
-            return -1
-        else:
-            return bucket[index][1]
-
+        index = self.hashkey(key)
+        curr = self.hashmap[index]   
+        while curr:
+            if curr.key == key:
+                return curr.val
+            curr = curr.next
+        return -1
+        
     def remove(self, key: int) -> None:
-        bucket, index = self.getIndex(key)
-        if index < 0:
-            return
-        else:
-            bucket.remove(bucket[index])
-    
-    def hashKey(self, key):
-        return key % self.size
-    
-    # find the target bucket and index of the key
-    # if index > 0, then (key, value) exist
-    # if index < 0, then (key, value) not exist
-    def getIndex(self, key):
-        hashResult = self.hashKey(key)
-        bucket = self.buckets[hashResult]
+        index = self.hashkey(key)
+        curr = self.hashmap[index]
+        while curr and curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
         
-        for i, (k, v) in enumerate(bucket):
-            if k == key:
-                return bucket, i
-        
-        return bucket, -1
-        
-
-
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
 # obj.put(key,value)
