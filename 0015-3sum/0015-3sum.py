d9d29ae4a2a2ad78ea23023f7effc1768d90651b
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-09 13:21:54 +0530

    Time: 1255 ms (80.61%), Space: 17.7 MB (90.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,17 +1,16 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = set()
-        seen = set()
         nums.sort()
         
         for index, a in enumerate(nums):
-            if a in seen:
+            if index > 0 and a == nums[index - 1]:
                 continue
-            seen.add(a)
-
+            
             left, right = index + 1, len(nums) - 1
             while left < right:
                 three_sum = a + nums[left] + nums[right]
+                
                 if three_sum > 0:
                     right -= 1
                 elif three_sum < 0:
@@ -19,4 +18,8 @@ class Solution:
                 else:
                     res.add((a, nums[left], nums[right]))
                     left += 1
-        return list(res)
+                    
+                    # #to avoid duplicates and never letting the left pass the right pointer
+                    # while nums[left] == nums[left - 1] and left < right:
+                    #     left += 1
+        return res
