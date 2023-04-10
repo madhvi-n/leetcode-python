617fc5aa96a9090d955082617afa199fd16b6efc
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-10 11:53:06 +0530

    Time: 36 ms (39.98%), Space: 13.9 MB (16.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -10,6 +10,6 @@ class Solution:
         for char in s:
             if stack and stack[-1] == hashmap.get(char):
                 stack.pop()
-            else:
-                stack.append(char)
+                continue
+            stack.append(char)
         return not stack
