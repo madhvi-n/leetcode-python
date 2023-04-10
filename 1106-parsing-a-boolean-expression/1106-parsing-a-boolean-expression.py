e503e3f2de1b0a0535903fedefec235240c30949
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-10 12:47:54 +0530

    Time: 51 ms (80.00%), Space: 14 MB (80.00%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
@@ -1,22 +1,33 @@
 class Solution:
     def parseBoolExpr(self, expression: str) -> bool:
-        hashmap = {'&' : all, '|' : any, '!' : lambda x : not x[0]}
-        
         stack = []
-        for c in expression:
-            if c in hashmap:
-                stack.append(hashmap[c])
-            elif c == '(':
-                stack.append('(')
-            elif c == 'f':
-                stack.append(False)
-            elif c == 't':
-                stack.append(True)
-                
-            elif c == ')':
-                ss = []
-                while stack[-1] != '(':
-                    ss.append(stack.pop())
+        for i in range(len(expression)):
+            if expression[i] == ')':
+                seen = set()
+                while stack and stack[-1] != '(':
+                    seen.add(stack.pop())
                 stack.pop()
-                stack.append(stack.pop()(ss))
-        return stack.pop()
+                operator = stack.pop()
+                
+                if operator == '&':
+                    if 'f' in seen:
+                        stack.append('f')
+                    else:
+                        stack.append('t')
+                
+                elif operator == '|':
+                    if 't' in seen:
+                        stack.append('t')
+                    else:
+                        stack.append('f')
+                
+                else:
+                    if 't' in seen:
+                        stack.append('f')
+                    else:
+                        stack.append('t')      
+                        
+            elif expression[i] != ',':
+                stack.append(expression[i])
+                
+        return stack[-1] == 't'
