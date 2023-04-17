d63d371fd1a1b55be3a4b329cd2ab1b77f2fa457
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-17 17:21:20 +0530

    Time: 58 ms (42.34%), Space: 14.6 MB (13.06%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -18,12 +18,14 @@ class Solution:
                 return True
             
             if nums[left] <= nums[mid]:
-                if target >= nums[left] and target < nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                # if target >= nums[left] and target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if target <= nums[right] and target > nums[mid]:
+                if nums[right] >= target > nums[mid]:
+                # if target <= nums[right] and target > nums[mid]:
                     left = mid + 1
                 else:
                     right = mid - 1
