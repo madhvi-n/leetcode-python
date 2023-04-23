7cc1c84e1451fa10b8d3c5c0417ef4413f602178
Madhvi Nandanwar <30744898+madhvi-n@users.noreply.github.com>
2023-04-23 11:08:41 +0530

    Time: 1294 ms (32.62%), Space: 277.2 MB (5.72%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,5 +1,12 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        # Base condition: Length of string is less than or equal to 1, return 0
+        # If string in memo, return value from memo
+        # Otherwise, compute as follows
+        # - if first and last characters are equal, recursive call function for s[1:n-1]
+        # - else recursively call function for s[1:n] and s[:n-1] and take min of the two plus 1 since we want min insertions
+        # at the end store the result in memo for current string and return
+        
         def min_insertions(s, memo={}):
             if s in memo:
                 return memo[s]
